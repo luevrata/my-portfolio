ccc2653848a8fd2d9561028614712437af28316e
Liza Firsovalu <luevrata@students.cs.ubc.ca>
2022-11-30 22:20:56 -0800

    tutorial

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-// import React from "react";
-// import {render}  from "react-dom";
-// import App from './App'
-// import './index.css'
-// render(<App/>, document.querySelector("#root"))
-
 import React from "react";
-import {createRoot}  from "react-dom/client";
+import {render}  from "react-dom";
 import App from './App'
 import './index.css'
-const container = document.querySelector("#root")
-const root = createRoot(container)
-root.render(<App/>)
+render(<App/>, document.querySelector("#root"))
+
+// import React from "react";
+// import {createRoot}  from "react-dom/client";
+// import App from './App'
+// import './index.css'
+// const container = document.querySelector("#root")
+// const root = createRoot(container)
+// root.render(<App/>)
 
 // import React from "react";
 // import Header from "./components/header/Header";
