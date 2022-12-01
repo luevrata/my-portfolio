f720fdb9bb3256ce298039f0625f6feb3fc149e4
elizaveta.firsova <elizaveta.firsova@workday.com>
2022-12-01 09:15:23 -0800

    tutorial

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM  from "react-dom";
 import App from './App'
 import './index.css'
 alert()
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<p>mew</p>, document.getElementById("root"))
 
 // import React from "react";
 // import {createRoot}  from "react-dom/client";
