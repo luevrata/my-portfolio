c118fb6dab10c1ea221b5fd463b788d446448c94
Liza Firsovalu <luevrata@students.cs.ubc.ca>
2022-12-01 00:02:44 -0800

    tutorial

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import App from './App'
 import './index.css'
+alert()
 ReactDOM.render(<App/>, document.getElementById("root"))
 
 // import React from "react";
