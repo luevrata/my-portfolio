f2469c000adf8457c85194ac2a2766e2155177d3
Liza Firsovalu <luevrata@students.cs.ubc.ca>
2022-11-30 21:54:35 -0800

    tutorial

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@
 // import {render}  from "react-dom";
 // import App from './App'
 // import './index.css'
-
 // render(<App/>, document.querySelector("#root"))
 
 import React from "react";
 import {createRoot}  from "react-dom/client";
 import App from './App'
 import './index.css'
-
-
 const container = document.querySelector("#root")
 const root = createRoot(container)
 root.render(<App/>)
+
+// import React from "react";
+// import Header from "./components/header/Header";
+// import Nav from "./components/nav/Nav";
+// import About from "./components/about/About";
+// import Experience from "./components/experience/Experience";
+// import Projects from "./components/projects/Projects";
+// import './index.css'
+
+// const App =() => {
+
+//     return(
+//     <div>
+//         <Header/>
+//         <Nav/>
+//         <About/>
+//         <Experience/>
+//         <Projects/>
+//     </div>
+//     )
+
+// }
+
+// export default App
